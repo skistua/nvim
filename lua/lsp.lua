ae74be67724e8a46f9b9e3fa602803140d41d499
Skistua <b166er.dada@gmail.com>
2022-03-13 22:27:02 +0800

    [modified] new signature help . lsp auto installer

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,17 +1,16 @@
 local nvim_lsp = require('lspconfig')
-local signature_help =  require "lsp_signature"
+
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    signature_help.on_attach()
 
     -- Mappings.
     local opts = { noremap=true, silent=true }
 
     buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', 'g\\', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
     buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
     buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
@@ -30,92 +29,24 @@ local on_attach = function(client, bufnr)
 end
 
 -- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+--local capabilities = vim.lsp.protocol.make_client_capabilities()
+--capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-nvim_lsp.pyright.setup {
-    on_attach = on_attach,
-    capabilities = capabilities
-}
+-- LSP Installer 
 
-local pid = vim.fn.getpid()
+local lsp_installer = require("nvim-lsp-installer")
 
-local omnisharp_bin = "~/lspServers/omnisharp/run"
-if vim.fn.has('win32') then
-    omnisharp_bin =  "C:/Users/b166e/lspServers/omnisharp/OmniSharp.exe"
-end
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+        on_attach = on_attach
+    }
+    server:setup(opts)
+end)
 
-nvim_lsp.omnisharp.setup{
-    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) };
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
-
-local rustanalyzer_bin = "C:/Users/b166e/lspServers/rust-analyzer.exe"
-nvim_lsp.rust_analyzer.setup{
-    cmd = {rustanalyzer_bin},
-    on_attach = on_attach,
-}
 
 -- luasnip setup
 local luasnip = require 'luasnip'
 
--- lsp signature_help
-
---[[
-
-signatureCfg = {
-  debug = false, -- set to true to enable debug logging
-  log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", -- log dir when debug is on
-  -- default is  ~/.cache/nvim/lsp_signature.log
-  verbose = false, -- show debug line number
-
-  bind = true, -- This is mandatory, otherwise border config won't get registered.
-               -- If you want to hook lspsaga or other signature handler, pls set to false
-  doc_lines = 10, -- will show two lines of comment/doc(if there are more than two lines in doc, will be truncated);
-                 -- set to 0 if you DO NOT want any API comments be shown
-                 -- This setting only take effect in insert mode, it does not affect signature help in normal
-                 -- mode, 10 by default
-
-  floating_window = true, -- show hint in a floating window, set to false for virtual text only mode
-
-  floating_window_above_cur_line = true, -- try to place the floating above the current line when possible Note:
-  -- will set to true when fully tested, set to false will use whichever side has more space
-  -- this setting will be helpful if you do not want the PUM and floating win overlap
-
-  floating_window_off_x = 1, -- adjust float windows x position.
-  floating_window_off_y = 1, -- adjust float windows y position.
-
-
-  fix_pos = false,  -- set to true, the floating window will not auto-close until finish all parameters
-  hint_enable = true, -- virtual hint enable
-  hint_prefix = "🐼 ",  -- Panda for parameter
-  hint_scheme = "String",
-  hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
-  max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
-                   -- to view the hiding contents
-  max_width = 80, -- max_width of signature floating_window, line will be wrapped if exceed max_width
-  handler_opts = {
-    border = "rounded"   -- double, rounded, single, shadow, none
-  },
-
-  always_trigger = false, -- sometime show signature on new line or in middle of parameter can be confusing, set it to false for #58
-
-  auto_close_after = nil, -- autoclose signature float win after x sec, disabled if nil.
-  extra_trigger_chars = {}, -- Array of extra characters that will trigger signature completion, e.g., {"(", ","}
-  zindex = 200, -- by default it will be on top of all floating windows, set to <= 50 send it to bottom
-
-  padding = '', -- character to pad on left and right of signature can be ' ', or '|'  etc
-
-  transparency = nil, -- disabled by default, allow floating win transparent value 1~100
-  shadow_blend = 36, -- if you using shadow as border use this set the opacity
-  shadow_guibg = 'Black', -- if you using shadow as border use this set the color e.g. 'Green' or '#121315'
-  timer_interval = 200, -- default timer check interval set to lower value if you want to reduce latency
-  toggle_key = nil -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
-}
-signature_help.setup(signature_help)
-
---]]
 
 -- nvim-cmp setup
 local kind_icons = {
@@ -194,6 +125,8 @@ cmp.setup {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = "buffer" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "path" },
     },
 }
 
--- vim-plug/plugins.vim
+++ vim-plug/plugins.vim
@@ -1,31 +1,26 @@
 call plug#begin('~/.vim/plugged')
 Plug 'easymotion/vim-easymotion'
 Plug 'vim-airline/vim-airline'
-"Plug 'junegunn/fzf'
-"Plug 'junegunn/fzf.vim'
 Plug 'morhetz/gruvbox'
 Plug 'mhinz/vim-startify'
 Plug 'preservim/nerdtree'
 Plug 'preservim/nerdcommenter'
 Plug 'jiangmiao/auto-pairs'
 Plug 'mhinz/vim-signify'
-"Plug 'liuchengxu/vim-which-key'
 Plug 'ryanoasis/vim-devicons'
 Plug 'kyazdani42/nvim-web-devicons'
-"Plug 'neoclide/coc.nvim', {'branch': 'release'}
-"Plug 'OmniSharp/omnisharp-vim'
-"Plug 'dense-analysis/ale'
-"Plug 'Yggdroot/LeaderF', { 'do': ':LeaderfInstallCExtension' }
 Plug 'tpope/vim-surround'
 Plug 'iamcco/markdown-preview.nvim', { 'do': { -> mkdp#util#install() }, 'for': ['markdown', 'vim-plug']}
 Plug 'ferrine/md-img-paste.vim'
 Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/nvim-cmp' " Autocompletion plugin
+Plug 'williamboman/nvim-lsp-installer' "Auto Lang Server Installer
 Plug 'hrsh7th/cmp-nvim-lsp' " LSP source for nvim-cmp
 Plug 'hrsh7th/cmp-buffer' "cmp buffer source
+Plug 'hrsh7th/cmp-nvim-lsp-signature-help' "signature help
+Plug 'hrsh7th/cmp-path' "path cmp help
 Plug 'saadparwaiz1/cmp_luasnip'
 Plug 'L3MON4D3/LuaSnip' " Snippets plugin
-Plug 'ray-x/lsp_signature.nvim' " lsp signature helper
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}  " We recommend updating the parsers on update
@@ -41,11 +36,7 @@ call plug#end()
 if has('win32')
     source ~/AppData/local/nvim/themes/gruvbox.vim
     source ~/AppData/local/nvim/themes/airline.vim
-    "source ~/AppData/local/nvim/keys/which-key.vim
     source ~/AppData/local/nvim/plug-config/start-screen.vim
-    "source ~/AppData/local/nvim/plug-config/coc.vim
-    "source ~/AppData/local/nvim/plug-config/omnisharp.vim
-    "source ~/AppData/local/nvim/plug-config/leaderf.vim
     source ~/AppData/local/nvim/plug-config/nerdtree.vim
     source ~/AppData/local/nvim/plug-config/markdown.vim
     source ~/AppData/local/nvim/plug-config/ims.vim
@@ -54,11 +45,7 @@ if has('win32')
 else
     source ~/.config/nvim/themes/gruvbox.vim
     source ~/.config/nvim/themes/airline.vim
-    "source ~/.config/nvim/keys/which-key.vim
     source ~/.config/nvim/plug-config/start-screen.vim
-    "source ~/.config/nvim/plug-config/coc.vim
-    "source ~/.config/nvim/plug-config/omnisharp.vim
-    "source ~/.config/nvim/plug-config/leaderf.vim
     source ~/.config/nvim/plug-config/nerdtree.vim
     source ~/.config/nvim/plug-config/markdown.vim
     source ~/.config/nvim/plug-config/ims.vim
